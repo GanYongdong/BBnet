7c4a09169aa0ee4e35528f336f9a77507ece496d
ganyongdong <1141951289@qq.com>
2020-11-18 18:50:07 +0800

    1118

---
+++ net/utils/dist_util.py
@@ -0,0 +1,15 @@
+import torch.distributed as dist
+
+def synchronize():
+    """
+       Helper function to synchronize (barrier) among all processes when
+       using distributed training
+    """
+    if not dist.is_available():
+        return 0
+    if not dist.is_initialized():
+        return 0
+    world_size = dist.get_world_size()
+    if world_size == 1:
+        return
+    dist.barrier()
--- train.py
+++ train.py
@@ -5,17 +5,17 @@ import torch
 import torch.distributed as dist
 
 from net.utils.misc import str2bool
+from net.utils.dist_util import synchronize
+from net.config import cfg
+from net.utils.misc import mkdir
 
 def main():
     print('hello main')
     
     parser = argparse.ArgumentParser(description='net')
     parser.add_argument(
-        "--config-file",
-        default="configs/vgg_ssd300_voc0712.yaml",
-        metavar="FILE",
-        help="path to config file",
-        type=str,
+        "--config-file", default="configs/vgg_ssd300_voc0712.yaml",
+        metavar="FILE", help="path to config file", type=str,
     )
     parser.add_argument("--local_rank", type=int, default=0)
     parser.add_argument('--log_step', default=10, type=int, help='Print logs every log_step')
@@ -23,16 +23,12 @@ def main():
     parser.add_argument('--eval_step', default=2500, type=int, help='Evaluate dataset every eval_step, disabled when eval_step < 0')
     parser.add_argument('--use_tensorboard', default=True, type=str2bool)
     parser.add_argument(
-        "--skip-test",
-        dest="skip_test",
-        help="Do not test the final model",
-        action="store_true",
+        "--skip-test", dest="skip_test", 
+        help="Do not test the final model", action="store_true",
     )
     parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
+        "opts", help="Modify config options using the command-line",
+        default=None, nargs=argparse.REMAINDER,
     )
     args = parser.parse_args()
     num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
@@ -45,7 +41,16 @@ def main():
         torch.cuda.set_device(args.local_rank)
         torch.distributed.init_process_group(backend="nccl", init_method="env://")
         synchronize()
-        
+    
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+
+    if cfg.OUTPUT_DIR:
+        mkdir(cfg.OUTPUT_DIR)
+
+    logger = setup
+
     print('finish main')
 
 if __name__ == '__main__':
